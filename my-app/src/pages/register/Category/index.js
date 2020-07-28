e5c08fce7aed6ff839e79d37d37108e92973ee6a
Victor <victor.as.nascimento@gmail.com>
2020-07-28 17:51:06 -0300

    fix

--- my-app/src/pages/register/Category/index.js
+++ my-app/src/pages/register/Category/index.js
@@ -2,14 +2,28 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault'
 
-function RegisterCategory(){
-  return(
-      <PageDefault>
-        <h1>Register Category</h1>
-        <Link to="/">
-            Home
+function RegisterCategory() {
+  return (
+    <PageDefault>
+      <h1>Register Category</h1>
+
+      <form>
+
+        <label>
+          Category Name:
+          <input
+            type="text"
+          />
+        </label>
+
+        <button>
+          Register
+        </button>
+      </form>
+      <Link to="/">
+        Home
         </Link>
-      </PageDefault>
+    </PageDefault>
   );
 }
 
