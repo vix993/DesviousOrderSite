8a6e2f8132b1e783144d0d2cce44b9e686d5ec89
Victor <victor.as.nascimento@gmail.com>
2020-08-01 13:59:14 -0300

    color scheme

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 export const FooterBase = styled.footer`
   background: var(--black);
-  border-top: 2px solid var(--primarydesvious);
+  border-top: 2px solid var(--secondarydesvious);
   padding-left: 16px;
   padding-right: 16px;
   padding-top: 32px;
